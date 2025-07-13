d6cf64de08efafcff14cda4e18ee788cba8069aa
Aang Solihin <angslhn@gmail.com>
2025-07-13 18:59:40 +0700

    AS Music Player - Menambahkan fitur keluar dan perbaikan beberapa logic.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 import time
+import keyboard
 
 def header() -> None:     
     print("─" * 84)
@@ -16,9 +17,7 @@ def header() -> None:
     print("─" * 84)
 
 def find_songs(path: str) -> list[str] :    
-    file_songs = [song for song in os.listdir(path) if song.endswith(".mp3")]
-    
-    return file_songs
+    return [song for song in os.listdir(path) if song.endswith(".mp3")]
 
 def selection_sort(songs: list[str]) -> list[str]:
     n = len(songs)
@@ -35,24 +34,33 @@ def selection_sort(songs: list[str]) -> list[str]:
     return songs
 
 def list_songs(songs: list[str]) -> None:
-    print("  Daftar Lagu")
+    print("  Daftar Lagu Tersedia")
     for no, song in enumerate(songs, start = 1):
         print(f"   {no}. {song[0:-4]}")
     
 def play_songs(selected: int, path: str, stack_songs: list[str]):
+    print("  [INFO] Tekan ESC jika anda ingin menghentikan pemutaran.")
+    
+    pygame.mixer.init()
+    
     index = selected - 1
     
     while index < len(stack_songs):
         song = stack_songs[index]
         song_path = os.path.join(path, song)
         
-        print(f"  ~ Memutar - {song[0:-4]}")
+        print(f"\n  ~ Memutar -> {song[0:-4]}")
         
         pygame.mixer.music.load(song_path)
         pygame.mixer.music.play()
-        
+            
         while pygame.mixer.music.get_busy():
-            time.sleep(1)
+            if keyboard.is_pressed("esc"):
+                pygame.mixer.music.stop()
+                print("\n  ~ Pemutaran lagu dihentikan.")
+                return
+            
+            time.sleep(0.1)
             
         index += 1
     
@@ -63,8 +71,6 @@ def main():
     
     if not os.path.exists(songs_path):
         os.mkdir(songs_path)
-        
-    pygame.mixer.init()
     
     songs = find_songs(songs_path)
     
@@ -91,6 +97,8 @@ def main():
     print("─" * 84)
     
     play_songs(selected, songs_path, stack_songs)
+    
+    print("─" * 84)
     
 
 if __name__ == "__main__":
